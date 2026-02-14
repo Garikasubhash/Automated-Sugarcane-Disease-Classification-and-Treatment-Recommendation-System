13e0d20fc3b96fbfb8dfea74ed13cf03eba5d8be
Garika Subhash <151701042+Garikasubhash@users.noreply.github.com>
2026-02-14 13:38:50 +0530

    Add files via upload

---
+++ Contact us/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ContactUs from "./pages/ContactUs.jsx";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
